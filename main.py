d73f5f1d0b0fe89effc51705aff75ad7e9cd7da0
Emiliano <emilianogtlp@gmail.com>
2023-04-02 19:50:00 -0700

    Added connection both to reddit api and PushshiftAPI

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
-import os
-from dotenv import load_dotenv
 import requests
+import datetime
+import oauth
+import os
+from pmaw import PushshiftAPI
 
-load_dotenv()
+def try_conn(token):
+    headers = {"User-Agent": "de-medium 0.1", "Authorization":"Bearer "+token} 
+    response = requests.get("https://oauth.reddit.com/api/v1/me", headers=headers)
+    return(response.json())
 
-key = os.environ.get('CLIENT_ID')
-secret = os.environ.get('SECRET_KEY')
-emiliano_pass = os.environ.get('EMILIANO')
+def get_posts(subreddit):
+    api = PushshiftAPI(num_workers=os.cpu_count()*5)
+    gen = api.search_submissions(after='2d',
+                                  before='1d',
+                                  subreddit=subreddit,
+                                  )
+    return list(gen)
 
-auth = requests.auth.HTTPBasicAuth(key,secret)
-
-data = {
-    'grant_type':'password',
-    'username':'emilianogtlp',
-    'password':emiliano_pass
-}
-
-headers = {'User-Agent':'de-medium/0.0.1'}
-res = requests.post('https://reddit.com/api/v1/access_token',auth=auth,data=data,headers=headers)
-print(res.json())
+if __name__ == "__main__":
+    #token = oauth.get_token()
+    #posts = try_conn(token=token)
+    #print(posts)
+    subreddit = 'mexico'
+    posts = get_posts(subreddit)
+    post_lists = [{"title":post["title"],"selftext":post["selftext"],"link_flair_text":post["link_flair_text"],"author":post["author"],"created_utc":post["created_utc"]} for post in posts]
+    for p in post_lists:
+        print(p)
+        print()
